ded7db6429a22a5c66a1cc7acdba6e2f54d210d5
Arieh Norton <arieh.norton@gmail.com>
2022-12-01 11:43:31 +0200

    new transaction

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                autocorrect: true,
+                decoration: const InputDecoration(labelText: 'Title'),
+                controller: titleController,
+              ),
+              TextField(
+                autocorrect: true,
+                decoration: const InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+              ),
+              TextButton(
+                  onPressed: (() {
+                    addTx(
+                      titleController.text,
+                      double.parse(amountController.text),
+                    );
+                  }),
+                  style: TextButton.styleFrom(foregroundColor: Colors.purple),
+                  child: const Text('Add Transaction'))
+            ]),
+      ),
+    );
+  }
+}
